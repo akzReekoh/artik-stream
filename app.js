b31ee1d2b41536c42effef15b256a74d05172c6f
Achilles Dinglasan <adinglasan@reekoh.com>
2017-03-21 17:39:03 +0800

    migrated to v3

--- app.js
+++ app.js
@@ -1,148 +1,144 @@
-'use strict';
+'use strict'
 
-const TOKEN_ENDPOINT       = 'https://accounts.artik.cloud/token',
-	  ARTIK_CLOUD_ENDPOINT = 'https://api.artik.cloud/v1.1';
+const TOKEN_ENDPOINT = 'https://accounts.artik.cloud/token'
+const ARTIK_CLOUD_ENDPOINT = 'https://api.artik.cloud/v1.1'
 
-var get      = require('lodash.get'),
-	async    = require('async'),
-	request  = require('request'),
-	platform = require('./platform'),
-	isEmpty  = require('lodash.isempty'),
-	config;
+let reekoh = require('reekoh')
+let _plugin = new reekoh.plugins.Stream()
+let get = require('lodash.get')
+let async = require('async')
+let request = require('request')
+let isEmpty = require('lodash.isempty')
 
-let processDeviceData = function (data, callback) {
-	async.each(data, (data, cb) => {
-		platform.requestDeviceInfo(data.sdid, function (error, requestId) {
-			if (error) return cb(error);
+let processDeviceData = (data, callback) => {
+  async.each(data, (data, cb) => {
+    _plugin.requestDeviceInfo(data.sdid)
+      .then((deviceInfo) => {
+        if (!deviceInfo) return cb(new Error(`Device ${data.sdid} is not registered.`))
 
-			platform.once(requestId, function (deviceInfo) {
-				if (!deviceInfo)
-					return cb(new Error(`Device ${data.sdid} not registered`));
+        _plugin.pipe(data)
+          .then(() => {
+            cb()
+          })
+          .catch((error) => {
+            cb(error)
+          })
+      })
+      .catch((error) => {
+        cb(error)
+      })
+  }, callback)
+}
 
-				platform.processData(data.sdid, JSON.stringify(data), cb);
-			});
-		});
-	}, callback);
-};
+_plugin.on('sync', () => {
+  let startDate = null
+  let endDate = Date.now()
 
-/**
- * Emitted when the platform issues a sync request. Means that the stream plugin should fetch device data from the 3rd party service.
- * @param {date} lastSyncDate Timestamp from when the last sync happened. Allows you to fetch data from a certain point in time.
- */
-platform.on('sync', function (lastSyncDate) {
-	let startDate = new Date(lastSyncDate).getTime();
-	let endDate = Date.now();
+  _plugin.getState()
+    .then((state) => {
+      startDate = state.lastSyncDate
+    })
+    .catch((err) => {
+      return _plugin.logException(err)
+    })
 
-	async.waterfall([
-		(done) => {
-			request.post({
-				url: TOKEN_ENDPOINT,
-				headers: {
-					'content-type': 'application/x-www-form-urlencoded'
-				},
-				form: {
-					grant_type: 'client_credentials'
-				},
-				json: true,
-				auth: {
-					user: config.client_id,
-					pass: config.client_secret
-				}
-			}, (error, response, body) => {
-				if (error)
-					done(error);
-				else if (body.error || response.statusCode !== 200)
-					done(new Error(body.error.message || body.error));
-				else if (isEmpty(body.access_token))
-					done(new Error('Invalid Credentials. No access token was received.'));
-				else
-					done(null, body.access_token);
-			});
-		},
-		(token, done) => {
-			let devices = [];
-			let hasMoreResults = true;
-			let offset = 0;
+  async.waterfall([
+    (done) => {
+      request.post({
+        url: TOKEN_ENDPOINT,
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+          'grant_type': 'client_credentials'
+        },
+        json: true,
+        auth: {
+          user: _plugin.config.clientId,
+          pass: _plugin.config.clientSecret
+        }
+      }, (error, response, body) => {
+        if (error) done(error)
+        else if (body.error || response.statusCode !== 200) done(new Error(body.error.message || body.error))
+        else if (isEmpty(body['access_token'])) done(new Error('Invalid Credentials. No access token was received.'))
+        else done(null, body['access_token'])
+      })
+    },
+    (token, done) => {
+      let devices = []
+      let hasMoreResults = true
+      let offset = 0
 
-			async.whilst(() => {
-				return hasMoreResults;
-			}, (cb) => {
-				request({
-					url: `${ARTIK_CLOUD_ENDPOINT}/users/${config.user_id}/devices?offset=${100 * offset}&count=100`,
-					json: true,
-					auth: {
-						bearer: token
-					}
-				}, (error, response, body) => {
-					if (error)
-						cb(error);
-					else if (body.error || response.statusCode !== 200)
-						done(new Error(body.error.message || body.error));
-					else {
-						let devicesTmp = get(body, 'data.devices');
+      async.whilst(() => {
+        return hasMoreResults
+      }, (cb) => {
+        request({
+          url: `${ARTIK_CLOUD_ENDPOINT}/users/${_plugin.config.userId}/devices?offset=${100 * offset}&count=100`,
+          json: true,
+          auth: {
+            bearer: token
+          }
+        }, (error, response, body) => {
+          if (error) cb(error)
+          else if (body.error || response.statusCode !== 200) done(new Error(body.error.message || body.error))
+          else {
+            let devicesTmp = get(body, 'data.devices')
 
-						if (isEmpty(devicesTmp)) {
-							hasMoreResults = false;
-							return cb();
-						}
+            if (isEmpty(devicesTmp)) {
+              hasMoreResults = false
+              return cb()
+            }
 
-						offset++;
+            offset++
 
-						async.map(devicesTmp, (device, next) => {
-							next(null, device.id);
-						}, (mapError, devicesIds) => {
-							devices = devices.concat(devicesIds);
-							cb();
-						});
-					}
-				});
-			}, (err) => {
-				done(err, token, devices);
-			});
-		},
-		(token, devices, done) => {
-			async.each(devices, (device, cb) => {
-				request({
-					url: `${ARTIK_CLOUD_ENDPOINT}/messages?count=100&startDate=${startDate}&endDate=${endDate}&sdid=${device}`,
-					json: true,
-					auth: {
-						bearer: token
-					}
-				}, (error, response, body) => {
-					if (error)
-						cb(error);
-					else if (body.error || response.statusCode !== 200)
-						done(new Error(body.error.message || body.error));
-					else {
-						let data = get(body, 'data');
+            async.map(devicesTmp, (device, next) => {
+              next(null, device.id)
+            }, (mapError, devicesIds) => {
+              devices = devices.concat(devicesIds)
+              cb()
+            })
+          }
+        })
+      }, (err) => {
+        done(err, token, devices)
+      })
+    },
+    (token, devices, done) => {
+      async.each(devices, (device, cb) => {
+        request({
+          url: `${ARTIK_CLOUD_ENDPOINT}/messages?count=100&startDate=${startDate}&endDate=${endDate}&sdid=${device}`,
+          json: true,
+          auth: {
+            bearer: token
+          }
+        }, (error, response, body) => {
+          if (error) cb(error)
+          else if (body.error || response.statusCode !== 200) done(new Error(body.error.message || body.error))
+          else {
+            let data = get(body, 'data')
 
-						if (isEmpty(data))
-							cb();
-						else
-							processDeviceData(data, cb);
-					}
-				});
-			}, done);
-		}
-	], (error) => {
-		if (error) platform.handleException(error);
-	});
-});
+            if (isEmpty(data)) cb()
+            else processDeviceData(data, cb)
+          }
+        })
+      }, done)
+    }
+  ], (error) => {
+    if (error) return _plugin.logException(error)
 
-/**
- * Emitted when the platform shuts down the plugin. The Stream should perform cleanup of the resources on this event.
- */
-platform.once('close', function () {
-	platform.notifyClose();
-});
+    _plugin.setState({lastSyncDate: endDate})
+      .then(() => {
+        //  do nothing
+      })
+      .catch((err) => {
+        _plugin.logException(err)
+      })
+  })
+})
 
-/**
- * Emitted when the platform bootstraps the plugin. The plugin should listen once and execute its init process.
- * Afterwards, platform.notifyReady() should be called to notify the platform that the init process is done.
- * @param {object} options The parameters or options. Specified through config.json.
- */
-platform.once('ready', function (options) {
-	config = options;
-	platform.notifyReady();
-	platform.log('Artik Stream has been initialized.');
-});
+_plugin.once('ready', () => {
+  _plugin.log('Artik Stream has been initialized.')
+  _plugin.emit('init')
+})
+
+module.exports = _plugin
--- test/artik-stream.test.js
+++ test/artik-stream.test.js
@@ -1,81 +1,135 @@
-'use strict';
+'use strict'
 
-var cp     = require('child_process'),
-	assert = require('assert'),
-	artikStream;
+const amqp = require('amqplib')
 
-describe('Stream', function () {
-	this.slow(5000);
+let _channel = null
+let _conn = null
+let app = null
 
-	after('terminate child process', function (done) {
-		console.log('Running after');
-		this.timeout(20000);
+describe('Artik Stream Test', () => {
+  before('init', () => {
+    process.env.ACCOUNT = 'adinglasan'
+    process.env.CONFIG = JSON.stringify({
+      clientId: 'b13c60dd1f264224a34d1e9c3d44ec27',
+      clientSecret: '7c08cea3447442acb7bef36dcdb99fb6',
+      userId: 'a103bd5381bc4d18b8dee8a728a5e0a2'
+    })
+    process.env.OUTPUT_PIPES = 'op.artik1, op.artik2'
+    process.env.PLUGIN_ID = 'artik.stream'
+    process.env.COMMAND_RELAYS = 'cr1, cr2'
+    process.env.LOGGERS = 'logger1, logger2'
+    process.env.EXCEPTION_LOGGERS = 'ex.logger1, ex.logger2'
+    process.env.BROKER = 'amqp://guest:guest@127.0.0.1/'
 
-		setTimeout(() => {
-			console.log('Killing...');
-			artikStream.kill('SIGKILL');
-			done();
-		}, 19000);
-	});
+    amqp.connect(process.env.BROKER)
+      .then((conn) => {
+        _conn = conn
+        return conn.createChannel()
+      }).then((channel) => {
+      _channel = channel
+    }).catch((err) => {
+      console.log(err)
+    })
+  })
 
-	describe('#spawn', function () {
-		it('should spawn a child process', function () {
-			assert.ok(artikStream = cp.fork(process.cwd()), 'Child process not spawned.');
-		});
-	});
+  after('close connection', function (done) {
+    _conn.close()
+    done()
+  })
 
-	describe('#handShake', function () {
-		it('should notify the parent process when ready within 5 seconds', function (done) {
-			this.timeout(5000);
+  describe('#start', function () {
+    it('should start the app', function (done) {
+      this.timeout(10000)
+      app = require('../app')
+      app.once('init', done)
+    })
+  })
 
-			artikStream.on('message', function (message) {
-				if (message.type === 'ready')
-					done();
-				else if (message.type === 'requestdeviceinfo') {
-					if (message.data.deviceId === 'fc8ecc16e42446be9411b07d2f10c260' ||
-						message.data.deviceId === '5ccc04ef21d246c4bdb68373b7289433' ||
-						message.data.deviceId === 'cd563817829f407f81928a551d869f1d') {
-						
-						artikStream.send({
-							type: message.data.requestId,
-							data: {
-								_id: message.data.deviceId
-							}
-						});
-					} else {
-						artikStream.send({
-							type: message.data.requestId
-						});
-					}
-				}
-				else if (message.type === 'data') {
-					console.log(message.data);
-				}
-			});
+  describe('#sync', () => {
+    it('should sync latest data of every device', function (done) {
+      done()
+    })
+  })
+})
 
-			artikStream.send({
-				type: 'ready',
-				data: {
-					options: {
-						client_id: 'b13c60dd1f264224a34d1e9c3d44ec27',
-						client_secret: '7c08cea3447442acb7bef36dcdb99fb6',
-						user_id: 'a103bd5381bc4d18b8dee8a728a5e0a2'
-					}
-				}
-			}, function (error) {
-				assert.ifError(error);
-			});
-		});
-
-		describe('#sync', function () {
-			it('should sync latest data of every device', function (done) {
-				artikStream.send({
-					type: 'sync',
-					data: {
-						last_sync_dt: new Date('12-12-1970')
-					}
-				}, done);
-			});
-		});
-	});
-});
+// 'use strict';
+//
+// var cp     = require('child_process'),
+// 	assert = require('assert'),
+// 	artikStream;
+//
+// describe('Stream', function () {
+// 	this.slow(5000);
+//
+// 	after('terminate child process', function (done) {
+// 		console.log('Running after');
+// 		this.timeout(20000);
+//
+// 		setTimeout(() => {
+// 			console.log('Killing...');
+// 			artikStream.kill('SIGKILL');
+// 			done();
+// 		}, 19000);
+// 	});
+//
+// 	describe('#spawn', function () {
+// 		it('should spawn a child process', function () {
+// 			assert.ok(artikStream = cp.fork(process.cwd()), 'Child process not spawned.');
+// 		});
+// 	});
+//
+// 	describe('#handShake', function () {
+// 		it('should notify the parent process when ready within 5 seconds', function (done) {
+// 			this.timeout(5000);
+//
+// 			artikStream.on('message', function (message) {
+// 				if (message.type === 'ready')
+// 					done();
+// 				else if (message.type === 'requestdeviceinfo') {
+// 					if (message.data.deviceId === 'fc8ecc16e42446be9411b07d2f10c260' ||
+// 						message.data.deviceId === '5ccc04ef21d246c4bdb68373b7289433' ||
+// 						message.data.deviceId === 'cd563817829f407f81928a551d869f1d') {
+//
+// 						artikStream.send({
+// 							type: message.data.requestId,
+// 							data: {
+// 								_id: message.data.deviceId
+// 							}
+// 						});
+// 					} else {
+// 						artikStream.send({
+// 							type: message.data.requestId
+// 						});
+// 					}
+// 				}
+// 				else if (message.type === 'data') {
+// 					console.log(message.data);
+// 				}
+// 			});
+//
+// 			artikStream.send({
+// 				type: 'ready',
+// 				data: {
+// 					options: {
+// 						client_id: 'b13c60dd1f264224a34d1e9c3d44ec27',
+// 						client_secret: '7c08cea3447442acb7bef36dcdb99fb6',
+// 						user_id: 'a103bd5381bc4d18b8dee8a728a5e0a2'
+// 					}
+// 				}
+// 			}, function (error) {
+// 				assert.ifError(error);
+// 			});
+// 		});
+//
+// 		describe('#sync', function () {
+// 			it('should sync latest data of every device', function (done) {
+// 				artikStream.send({
+// 					type: 'sync',
+// 					data: {
+// 						last_sync_dt: new Date('12-12-1970')
+// 					}
+// 				}, done);
+// 			});
+// 		});
+// 	});
+// });
